b39e95ecbf331ca3936e523ac8bac10a6ab2e28f
TrekDev <TrekDev0@gmail.com>
2015-12-31 17:42:15 +0000

    Add separate platform configurations for each ABI

--- src/OpenCV.SDKDemo/Activities/Main.cs
+++ src/OpenCV.SDKDemo/Activities/Main.cs
@@ -10,12 +10,9 @@ namespace OpenCV.SDKDemo.Activities
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
             Button button;
-
             button = FindViewById<Button>(Resource.Id.cameraPreview);
             button.Click += (s, e) => StartActivity(typeof(CameraPreview));
         }
